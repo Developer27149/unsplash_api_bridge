29eecfe308986a0303a51242ef36688dfc0a4edc
youyi.qin <youyi.qin@shihengtech.com>
2022-04-18 01:01:59 +0800

    add: :tada: api test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath:
-    process.env.NODE_ENV === "development" ? undefined : "/unsplash_api_bridge",
-  assetPrefix: "/unsplash_api_bridge",
+  // basePath:
+  //   process.env.NODE_ENV === "development" ? undefined : "/unsplash_api_bridge",
+  // assetPrefix: "/unsplash_api_bridge",
 };
 
 module.exports = nextConfig;
