8ae188827f540535ae3b5844a7e76e4817124e59
youyi.qin <youyi.qin@shihengtech.com>
2022-04-18 00:55:15 +0800

    fix: :bug: gh pages css load

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath:
+    process.env.NODE_ENV === "development" ? undefined : "/unsplash_api_bridge",
+  assetPrefix: "/unsplash_api_bridge",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
